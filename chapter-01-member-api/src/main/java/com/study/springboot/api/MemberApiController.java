625d3236f06e58f725c3ffab3255bdf78eec507a
Heo Jong Tae <heo9910@naver.com>
2020-11-13 18:38:06 +0900

    .

--- chapter-01-member-api/src/main/java/com/study/springboot/api/MemberApiController.java
+++ chapter-01-member-api/src/main/java/com/study/springboot/api/MemberApiController.java
@@ -1,6 +1,7 @@
 package com.study.springboot.api;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,14 +20,33 @@ public class MemberApiController {
     
     private final MemberService memberService;
     
+    /**
+     * v1은 api처리에 엔티티 사용
+     * 엔티티가 변경되면 API스펙이 바뀌므로 아주 좋지 않은 방식!!! 사용하지 않을 것을 권장!!!!
+     */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
     }
     
+    /**
+     * v2는 엔티티가 변경되고 api스펙이 변경되지 않음 - 권장하는 방식!
+     */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member(request.getUserName());
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+    
     @Data
-    @RequiredArgsConstructor
+    static class CreateMemberRequest {
+        @NotEmpty
+        private String userName;
+    }
+    
+    @Data
     static class CreateMemberResponse {
         @NonNull
         private Long id;
